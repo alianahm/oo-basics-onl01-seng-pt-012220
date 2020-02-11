cf530dbe586aac03f32f1fb5713d53f792be16dd
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-02-11 05:19:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,24 +1,21 @@
-require 'pry'
-  attr_accessor :author, :page_count, :genre	
-  attr_reader :title	
-
-
 class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
-  def initialize(title)	  def initialize(title)
-    @title = title	    @title = title
-  end	  end
-
   GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
   def genre=(genre)
     @genre = genre
-    GENRES << genre unless GENRES.any? { |g| g == genre }
+    GENRES << genre
+    #or GENRES = @genre
   end
 
-
-  def turn_page	  def turn_page
-    puts "Flipping the page...wow, you read fast!"	    puts "Flipping the page...wow, you read fast!"
-
+end
